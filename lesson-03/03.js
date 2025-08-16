72143ac0ff30efc85716c3f889ab67d1e67f4595
ATRadlen <radlen.aks.work@gmail.com>
2025-08-16 20:49:13 +0200

    lesson_3-q1-3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,9 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(number_1, number_2, number_3) {
+    let largest = number_1;
+    if (number_2 > largest) { largest = number_2; }
+    if (number_3 > largest) { largest = number_3; }
+    return largest;
+}
+пше
